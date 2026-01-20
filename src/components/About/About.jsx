adfaa7d7449c605cfacccc2638fe6031d34dd59c
Akshit Dimri <akshitdimri31@gmail.com>
2026-01-20 14:54:08 +0530

    resume update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1cpPf6Zesgk5Sov7QnOb87utF3Qkk-zUj/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1yo3xTjEObk7eGV0IMScI7FrSIbc7SjgL/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
